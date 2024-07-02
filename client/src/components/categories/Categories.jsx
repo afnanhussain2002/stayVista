693e651d5d02498ffea84b4d8b6f643abde2f096
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-02 09:00:22 +0600

    Show a border bottom if user select any category

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,20 +1,27 @@
+import { useSearchParams } from "react-router-dom";
 import Container from "../Shared/Container";
-import { categories } from "./CategoriesData";
+import { categories } from "./CategoriesData.js";
 import CategoryBox from "./CategoryBox";
 
-
 const Categories = () => {
-    return (
-        <>
-            <Container>
-                <div className="pt-4 flex items-center justify-between overflow-x-auto">
-            {categories.map(category => <CategoryBox key={category.label} label={category.label} icon={category.icon}></CategoryBox> )}
-
-                </div>
-
-            </Container>
-        </>
-    );
+  const [params, setParams] = useSearchParams();
+  const category = params.get('category');
+  return (
+    <>
+      <Container>
+        <div className="pt-4 flex items-center justify-between overflow-x-auto">
+          {categories.map((item) => (
+            <CategoryBox
+              key={item.label}
+              label={item.label}
+              icon={item.icon}
+              selected={category === item.label}
+            ></CategoryBox>
+          ))}
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Categories;
--- client/src/components/categories/CategoryBox.jsx
+++ client/src/components/categories/CategoryBox.jsx
@@ -1,7 +1,33 @@
-const CategoryBox = ({ label, icon: Icon }) => {
+import {useNavigate, useSearchParams} from 'react-router-dom'
+import qs from 'query-string'
+
+const CategoryBox = ({ label, icon: Icon, selected }) => {
+const [params, setParams] = useSearchParams()
+const navigate = useNavigate()
+
+console.log(selected);
+
+const handleClick = () =>{
+    console.log(label);
+    let currentQuery = {}
+    if (params) {
+        currentQuery = qs.parse(params.toString())
+    }
+
+        const updatedQuery = {...currentQuery , category:label}
+
+        const url = qs.stringifyUrl({
+            url:'/',
+            query:updatedQuery
+        })
+        navigate(url)
+    
+    }
+    params.get('category')
+
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-3border-b-2 hover:text-neutral-800">
-      <Icon />
+    <div onClick={handleClick} className={`flex flex-col items-center justify-center gap-2 p-3border-b-2 hover:text-neutral-800 cursor-pointer border-b-2 ${selected ? 'border-b-neutral-800 text-neutral-800' : 'border-transparent text-neutral-500'}`}>
+      <Icon size={26}/>
       <div className="text-sm font-medium">{label}</div>
     </div>
   );
