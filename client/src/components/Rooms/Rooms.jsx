ce51eae8cbfd5d203b473a842198cd58000621bd
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-02 09:38:58 +0600

    Client: If category data was empty, show a text

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Container from "../Shared/Container";
 import RoomCard from "./RoomCard";
 import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/heading/Heading";
 
 
 const Rooms = () => {
@@ -23,9 +24,16 @@ const Rooms = () => {
     return (
         <div>
             <Container>
-                <div className="grid pt-4 gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+                {
+                    rooms && rooms.length > 0 ?
+                    <div className="grid pt-4 gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                     {rooms.map(room => <RoomCard room={room} key={room._id}></RoomCard>)}
                 </div>
+                :
+                <div className="flex items-center justify-center min-h-[calc(100vh-300px)]">
+                    <Heading title={'No Rooms Available in this Category'} center={ true } subtitle={'Please Select other Rooms'}/>
+                </div>
+                }
             </Container>
         </div>
     );
