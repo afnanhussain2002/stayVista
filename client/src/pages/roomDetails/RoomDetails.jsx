14356aa355a63c76b7b2a901d88d467046035897
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-02 18:50:20 +0600

    Client: add some header on room details page

--- client/src/pages/roomDetails/RoomDetails.jsx
+++ client/src/pages/roomDetails/RoomDetails.jsx
@@ -3,31 +3,40 @@ import Container from "../../components/Shared/Container";
 import { useEffect, useState } from "react";
 import Loader from "../../components/Shared/Loader";
 import { Helmet } from "react-helmet-async";
-
+import Header from "../../components/RoomDetails/Header/Header";
+import RoomInfo from "../../components/RoomDetails/RoomInfo/RoomInfo";
 
 const RoomDetails = () => {
-    const {id} = useParams()
-    const [loading, setLoading] = useState(false)
-    const [room, setRoom ] = useState({})
-    useEffect(() =>{
-        setLoading(true)
-        fetch('/rooms.json')
-        .then(res => res.json())
-        .then(data =>{
-            const singleRoom = data.find(item => item._id === id)
-            setRoom(singleRoom)
-            setLoading(false)
-        })
-    },[id])
-    if (loading) return <Loader/>
-    return (
-        <>
-            <Container>
-        <Helmet><title>{room.title}</title></Helmet>
-                <h3>{room.title}</h3>
-            </Container>
-        </>
-    );
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [room, setRoom] = useState({});
+  useEffect(() => {
+    setLoading(true);
+    fetch("/rooms.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const singleRoom = data.find((item) => item._id === id);
+        setRoom(singleRoom);
+        setLoading(false);
+      });
+  }, [id]);
+  if (loading) return <Loader />;
+  return (
+    <>
+      <Container>
+        <Helmet>
+          <title>{room.title}</title>
+        </Helmet>
+        <div>
+        <div className="flex flex-col gap-6"> <Header roomData={room}/> </div>
+        {/* <div className=""><RoomInfo roomData={room}/></div>j */}
+        {/* Calender */}
+        </div>
+
+     
+      </Container>
+    </>
+  );
 };
 
 export default RoomDetails;
