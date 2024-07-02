ec7d6480c359bfdbc784885dcbd0d1929b322d6f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-02 21:32:33 +0600

    Client: Add reseve button and try to showing total price

--- client/src/components/RoomDetails/RoomReservaton/RoomReservation.jsx
+++ client/src/components/RoomDetails/RoomReservaton/RoomReservation.jsx
@@ -1,15 +1,28 @@
+import Button from "../../Button/Button";
 import Calender from "../Calender/Calender";
 
-
-const RoomReservation = ({room}) => {
-    return (
-        <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
-            <div className="flex items-center gap-1 p-4">
-                <div className="text-2xl font-semibold">${room?.price} <span className="text-sm">Night</span></div>
-            </div>
-            <Calender/>
+const RoomReservation = ({ room }) => {
+  return (
+    <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
+      <div className="flex items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">
+          ${room?.price} <span className="text-sm">Night</span>
         </div>
-    );
+      </div>
+      <div className="flex justify-center">
+        <Calender />
+      </div>
+      <hr />
+      <div className="p-4">
+        <Button label={"Reserve"} />
+      </div>
+      <hr />
+      <div className="p-4 flex items-center justify-between font-semibold text-lg">
+        <div>Total</div>
+        <div>$ {room?.price}</div>
+      </div>
+    </div>
+  );
 };
 
 export default RoomReservation;
