2cdf9ee6af2660198b3d7c0bd482f2152d1bbdd3
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-02 09:14:45 +0600

    Client: Add category filter by rooms

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -1,19 +1,29 @@
 import { useEffect, useState } from "react";
 import Container from "../Shared/Container";
 import RoomCard from "./RoomCard";
+import { useSearchParams } from "react-router-dom";
 
 
 const Rooms = () => {
     const [rooms, setRooms] = useState([])
+    const [params, setParams] = useSearchParams()
+    const category = params.get('category')
     useEffect(() =>{
         fetch('./rooms.json')
         .then(res => res.json())
-        .then(data =>setRooms(data))
-    },[])
+        .then(data =>{
+            if (category) {
+                const filterCategory = data.filter(room => room.category === category)
+                setRooms(filterCategory)
+            }else{
+                setRooms(data)
+            }
+        })
+    },[category])
     return (
         <div>
             <Container>
-                <div className="grid pt-4 gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                <div className="grid pt-4 gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                     {rooms.map(room => <RoomCard room={room} key={room._id}></RoomCard>)}
                 </div>
             </Container>
--- client/src/components/categories/CategoryBox.jsx
+++ client/src/components/categories/CategoryBox.jsx
@@ -26,7 +26,7 @@ const handleClick = () =>{
     params.get('category')
 
   return (
-    <div onClick={handleClick} className={`flex flex-col items-center justify-center gap-2 p-3border-b-2 hover:text-neutral-800 cursor-pointer border-b-2 ${selected ? 'border-b-neutral-800 text-neutral-800' : 'border-transparent text-neutral-500'}`}>
+    <div onClick={handleClick} className={`flex flex-col items-center justify-center gap-2 p-3border-b-2 hover:text-neutral-800 cursor-pointer border-b-2 ${selected ? 'border-b-neutral-800 text-neutral-800' : ''}`}>
       <Icon size={26}/>
       <div className="text-sm font-medium">{label}</div>
     </div>
