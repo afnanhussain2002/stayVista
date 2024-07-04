61922590f61ab2364c83a818b5e9775789d5d4fb
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-04 12:56:35 +0600

    Add image text on image upload button when user select any image

--- client/src/components/Dashboard/Host/AddRoom.jsx
+++ client/src/components/Dashboard/Host/AddRoom.jsx
@@ -1,42 +1,81 @@
-import { Helmet } from "react-helmet-async"
+import { Helmet } from "react-helmet-async";
 import AddRoomForm from "../../Form/AddRoomForm";
 import { useState } from "react";
+import useAuth from "../../../hooks/useAuth";
+import { imageUpload } from "../../../api/utils";
 
 const AddRoom = () => {
-    const [dates, setDates] = useState({
-        startDate: new Date(),
-        endDate: new Date(),
-        key: 'selection',
-      })
-    const handleSubmit = async e =>{
-        e.preventDefault()
-        const form = e.target
-        const location = form.location.value
-        const category = form.category.value
-        const title = form.title.value
-        const to = dates.endDate
-        const from = dates.startDate
-        const price = form.price.value
-        const guests = form.total_guest.value
-        const bathrooms = form.bathrooms.value
-        const description = form.description.value
-        const bedrooms = form.bedrooms.value
-        const image = form.image.files[0]
-    }
-    // Handle Date change from reat-date-range calender
-    const handleDates = ranges =>{
-        setDates(ranges.selection)
-    }
-    return (
-        <>
-        <Helmet><title>Add Room | Dashboard</title></Helmet>
+  const { user } = useAuth();
+  const [loading, setLoading] = useState(false)
+  const [uploadButtonText, setUploadButtonText] = useState('Upload Image')
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const location = form.location.value;
+    const category = form.category.value;
+    const title = form.title.value;
+    const to = dates.endDate;
+    const from = dates.startDate;
+    const price = form.price.value;
+    const guests = form.total_guest.value;
+    const bathrooms = form.bathrooms.value;
+    const description = form.description.value;
+    const bedrooms = form.bedrooms.value;
+    const image = form.image.files[0];
+    const host = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+    const image_url = await imageUpload(image);
 
-        {/* Form */}
+    const roomData = {
+      location,
+      category,
+      title,
+      to,
+      from,
+      price,
+      guests,
+      bathrooms,
+      bedrooms,
+      host,
+      description,
+      image: image_url?.data?.display_url,
+    };
+    console.table(roomData);
+  };
+  // Handle Date change from react-date-range calender
+  const handleDates = (ranges) => {
+    setDates(ranges.selection);
+  };
+//   handle image button text
+const handleImageChange = image =>{
+    setUploadButtonText(image.name)
+}
+  return (
+    <>
+      <Helmet>
+        <title>Add Room | Dashboard</title>
+      </Helmet>
 
-        <AddRoomForm handleSubmit={handleSubmit} handleDates={handleDates} dates={dates}/>
-            
-        </>
-    );
+      {/* Form */}
+
+      <AddRoomForm
+        handleSubmit={handleSubmit}
+        handleDates={handleDates}
+        dates={dates}
+        handleImageChange={handleImageChange}
+        loading={loading}
+        uploadButtonText={uploadButtonText}
+      />
+    </>
+  );
 };
 
 export default AddRoom;
