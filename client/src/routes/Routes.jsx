0b3d852e2419638dfb110198d14eedcc028389e8
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-04 10:41:54 +0600

    Client: Add a Dashboard layout and also add a route for that

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,30 +1,36 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Main from '../layouts/Main'
-import Home from '../pages/Home/Home'
-import ErrorPage from '../pages/ErrorPage'
-import Login from '../pages/Login/Login'
-import SignUp from '../pages/SignUp/SignUp'
-import RoomDetails from '../pages/roomDetails/RoomDetails'
-import PrivateRoutes from './PrivateRoutes'
-import { getSingleRoom } from '../api/rooms'
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import RoomDetails from "../pages/roomDetails/RoomDetails";
+import PrivateRoutes from "./PrivateRoutes";
+import { getSingleRoom } from "../api/rooms";
+import DashboardLayouts from "../layouts/DashboardLayouts";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path:'/room/:id',
-        element:<PrivateRoutes><RoomDetails/></PrivateRoutes>,
-        loader:({params}) => getSingleRoom(params.id)
-      }
+        path: "/room/:id",
+        element: (
+          <PrivateRoutes>
+            <RoomDetails />
+          </PrivateRoutes>
+        ),
+        loader: ({ params }) => getSingleRoom(params.id),
+      },
     ],
   },
-  { path: '/login', element: <Login /> },
-  { path: '/signup', element: <SignUp /> },
-])
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <SignUp /> },
+  { path: "/dashboard", element: <DashboardLayouts />, children: [] },
+]);
