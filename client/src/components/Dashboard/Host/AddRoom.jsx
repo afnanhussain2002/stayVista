3fc2ffe863965eb32a8f7db49b6beb6289e212ee
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-07-04 12:30:49 +0600

    Cliet: Admin can now set start date and end date

--- client/src/components/Dashboard/Host/AddRoom.jsx
+++ client/src/components/Dashboard/Host/AddRoom.jsx
@@ -1,8 +1,14 @@
 import { Helmet } from "react-helmet-async"
 import AddRoomForm from "../../Form/AddRoomForm";
+import { useState } from "react";
 
 const AddRoom = () => {
-    const handleSubmit = e =>{
+    const [dates, setDates] = useState({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection',
+      })
+    const handleSubmit = async e =>{
         e.preventDefault()
         const form = e.target
         const location = form.location.value
@@ -17,13 +23,17 @@ const AddRoom = () => {
         const bedrooms = form.bedrooms.value
         const image = form.image.files[0]
     }
+    // Handle Date change from reat-date-range calender
+    const handleDates = ranges =>{
+        setDates(ranges.selection)
+    }
     return (
         <>
         <Helmet><title>Add Room | Dashboard</title></Helmet>
 
         {/* Form */}
 
-        <AddRoomForm handleSubmit={handleSubmit}/>
+        <AddRoomForm handleSubmit={handleSubmit} handleDates={handleDates} dates={dates}/>
             
         </>
     );
